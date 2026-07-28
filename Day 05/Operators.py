c3d43945a8582c0e653167711b27d0f23099e031
Samruddhi Nikhade <samruddhinikhade12@gmail.com>
2026-07-28 15:19:20 +0530

    Day 05 Conditional Statements, Operators and building calculator

--- Day 05/Operators.py
+++ Day 05/Operators.py
@@ -23,3 +23,38 @@ print(a is b)
 print(b is c)
 print(a is c)
 print(a is not c)
+
+
+# Bitwise Operators
+
+a = 10
+b = 8
+
+print("Binary val",bin(a))
+print("Binary val",bin(b))
+print("Result of ",a,"&",b,"is :",bin(a&b),"/",a&b)
+print("Result of ",a,"&|",b,"is :",bin(a|b),"/",a|b)
+print("Result of ",a,"^",b,"is :",bin(a^b),"/",a^b)
+
+
+# Let's build a calculator
+
+a = int(input("Enter a :- "))
+b = int(input("Enter b :- "))
+
+opr = input("Enter an operation (+ , - , * , / , % , **) :- ")
+
+if opr == '+':
+    print(a+b)
+elif opr == '-':
+    print(a-b)
+elif opr == '*':
+    print(a*b)
+elif opr == '/':
+    print(a/b)
+elif opr =='%':
+    print(a%b)
+elif opr == '**':
+    print(a**b)
+else :
+    print("Invalid operation!!")
