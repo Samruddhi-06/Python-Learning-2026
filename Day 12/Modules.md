9beb45c7bb86703d9ee31ef4a251961e6d1eaff9
Samruddhi Nikhade <samruddhinikhade12@gmail.com>
2026-08-05 15:36:37 +0530

    Day 12 Modules in python

---
+++ Day 12/Modules.md
@@ -0,0 +1,9 @@
+## Day 12
+
+1. Modules in python
+2. why do we use modules
+3. types of modules -> 1. built-in
+                       2. user-defined
+                       3. third-party
+4. ways to import modules
+5. how python finds modules
---
+++ Day 12/modules.py
@@ -0,0 +1,34 @@
+# How to import modules and use it
+
+# import whole module
+
+# import mymodules
+# print(mymodules.sum(65,55))
+
+# import specific function
+
+# from mymodules import mul
+# print(mul(11,5))
+
+# import with alias
+
+# import mymodules as m
+# print(m.sub(89,52))
+
+# import everything
+# from mymodules import *
+# print(sum(44,66))
+# print(mul(9,12))
+
+#  Math module
+
+import math
+
+print(math.sqrt(36),'sqrt')
+print(math.pow(5,3),'power')
+print(math.ceil(899.95),'ceil')
+print(math.floor(999.999),'floor')
+print(math.fabs(-111000.2352),'absolute')
+print(math.factorial(6),'factorial')
+print(math.log(10),'logarithm')
+print(math.sin(60),'sin60')
