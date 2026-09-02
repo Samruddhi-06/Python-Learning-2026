2231c30e6e86bda666e58061fa36fb40427f5d4c
Samruddhi Nikhade <samruddhinikhade12@gmail.com>
2026-09-02 15:12:52 +0530

    Day 21 static method, class vs instance variables and excersises

--- Day 21/Excersise_2.py
+++ Day 21/Excersise_2.py
@@ -1,3 +1,23 @@
 # Write a program to clear the clutter inside a folder on your computer.
 # You should use os module to rename all the png images from 1.png all till n.png 
 
+import os 
+
+folder = "Day 21\My_Folder"
+
+files = os.listdir(folder)
+
+print("Files before : ",files)
+
+count = 1
+
+for file in files:
+    if file.endswith(".png"):
+        old_path = os.path.join(folder,file)
+        new_path = os.path.join(folder, str(count) + ".png")
+        os.rename(old_path, new_path)
+        count += 1
+
+print()
+files = os.listdir(folder)
+print("Files after : ",files)
