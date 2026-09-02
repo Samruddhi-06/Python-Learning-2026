bc9df3915594aa26596517482c4ad86feed21760
Samruddhi Nikhade <samruddhinikhade12@gmail.com>
2026-09-02 15:34:39 +0530

    Day 22 Class Method, dir(), __dict__, help() methods

---
+++ Day 22/Class_Method_dir,dict,help_methods.py
@@ -0,0 +1,56 @@
+# class method
+
+class Employee:
+    company = "Apple"
+
+    def show(self, name):
+        self.name = name
+        print(f"The name is {self.name} and company is {self.company}")
+
+    @classmethod
+    def changeCompany(cls, newCompany):
+        cls.company = newCompany
+
+e1 = Employee()
+e1.show("Sam")
+e1.changeCompany("Tesla")
+e1.show("Sam")
+print(Employee.company)
+
+
+# class method as alternative constructor
+
+class Person:
+    def __init__(self, name, age, character):
+        self.name = name
+        self.age = age
+        self.character = character
+
+    @classmethod
+    def from_string(cls, String):
+        name,age, character = String.split(',')
+        return cls(name, int(age), character)
+
+person = Person.from_string("Jhon Doe,  30, he has got some skills" )
+print(person.name, person.age, person.character)
+
+
+
+# use of dir(), __dict__, help() 
+
+print("---------------------")
+
+x = [1,2,3,4,5,6]
+print(dir(x))
+print(x.__add__)
+
+class Person1:
+    def __init__(self, name, age, character):
+        self.name = name
+        self.age = age
+        self.character = character
+
+p = Person1("Jhon", 30, "he is nice")
+print(p.__dict__)
+
+print(help(Person1))
---
+++ Day 22/Class_Methods_dir,dict,help_methods.md
@@ -0,0 +1,5 @@
+## day 22
+
+1. class method
+2. class method as alternative of constructor
+3. dir(), __dict__, help() method
