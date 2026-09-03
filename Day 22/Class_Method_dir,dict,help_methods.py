b55b50b7d8e751facc306168c2c9259a56f28504
Samruddhi Nikhade <samruddhinikhade12@gmail.com>
2026-09-03 14:27:40 +0530

    Day 22 Class Method, class method as alternative constructor, dir(), __dict__, hepl()

--- Day 22/Class_Method_dir,dict,help_methods.py
+++ Day 22/Class_Method_dir,dict,help_methods.py
@@ -38,7 +38,7 @@ print(person.name, person.age, person.character)
 
 # use of dir(), __dict__, help() 
 
-print("---------------------")
+print("-------------------------------")
 
 x = [1,2,3,4,5,6]
 print(dir(x))
